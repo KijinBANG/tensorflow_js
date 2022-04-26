52f80f9b8b0763f8a9b2e49835ca4771572be8d4
KijinBANG <mr.kijin.bang@gmail.com>
2022-04-26 17:58:22 +0900

    study about making web server based on node.js environment.

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-import * as tf from '@tensorflow/tfjs';
+// import * as tf from '@tensorflow/tfjs';
+//
+// const model = tf.sequential();
+//
+// model.add(tf.layers.dense({units: 1, inputShape: [2]}));
+//
+// model.compile({loss:'meanSquaredError', optimizer:'adam'});
+//
+// const xs = tf.tensor2d([[0, 0], [0, 1], [1, 0], [1, 1]], [4, 2]);
+// const ys = tf.tensor2d([0, 1, 1, 0], [4, 1]);
+//
+// model.fit(xs, ys).then(() => {
+//     model.predict(tf.tensor2d([[0, 1]], [1, 2])).print();
+// });
 
-const model = tf.sequential();
+//web server 만들기
+import express from 'express';
+import path from 'path';
+const __dirname = path.resolve();
 
-model.add(tf.layers.dense({units: 1, inputShape: [2]}));
+const app = express();
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-model.compile({loss:'meanSquaredError', optimizer:'adam'});
+app.set('port', process.env.PORT || 3000);
 
-const xs = tf.tensor2d([[0, 0], [0, 1], [1, 0], [1, 1]], [4, 2]);
-const ys = tf.tensor2d([0, 1, 1, 0], [4, 1]);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
 
-model.fit(xs, ys).then(() => {
-    model.predict(tf.tensor2d([[0, 1]], [1, 2])).print();
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
 });
